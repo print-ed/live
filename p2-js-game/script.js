dfacd8fa7b56d8850319024b9dc518f54993f1b2
Edreyel Adora <edreyel.aaadora@gmail.com>
2022-09-30 13:24:14 +0800

    feat: add change screen on button click and fixed some issues with first page

---
+++ p2-js-game/script.js
@@ -0,0 +1,34 @@
+// Game display
+function startGame() {
+    let nameLength = document.getElementById("name")
+    // get the value user name and convert it to an array
+    let lengthContainer = nameLength.value.split('')
+    //then use if else to look if the array length is less than, equal, or more than the number given
+    if (lengthContainer.length === 0) {
+        alert("NAME IS EMPTY")
+    }
+    else if (lengthContainer.length > 15) {
+        alert("Y IS YOUR NAME TOO LONG? MAKE IT SHORTER!")
+    }
+    else if (lengthContainer.length < 2) {
+        alert("TOO SHORT")
+    }
+    // value above are false then go to next screen
+    else {
+        let start = document.getElementById("gameStart")
+        let ingame = document.getElementById("inGame")
+        let end = document.getElementById("inGame")
+        start.style.display = "none";
+        ingame.style.display = "block";
+        end.style.display = "none";
+    }
+}
+
+function gameOver() {
+    let start = document.getElementById("gameStart")
+    let ingame = document.getElementById("inGame")
+    let end = document.getElementById("inGame")
+    start.style.display = "none";
+    ingame.style.display = "none";
+    end.style.display = "block";
+}
